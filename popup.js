b0bc744dc3a33dc847f4c709463cb09690f6743b
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-09-03 19:42:17 +0300

    adding a service worker

--- popup.js
+++ popup.js
@@ -1,15 +1,4 @@
-const alertButton = document.querySelector(".alertButton");
-const timerBox = document.querySelector(".timer");
+import items from "./service-worker.js";
 
-let timeCount = 58000;
-
-const millisToMinutesAndSeconds = (millis) => {
-  const minutes = Math.floor(millis / 60000);
-  const seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-};
-
-setInterval(() => {
-  timeCount = timeCount + 1;
-  timerBox.innerHTML = millisToMinutesAndSeconds(timeCount);
-}, 1);
+console.log(items());
+items(document.querySelector(".timer").innerHTML);

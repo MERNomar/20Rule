79a51eb2eb9f1d4526653a345fd5fe3ea7c64830
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-09-04 17:41:43 +0300

    Added Audio every five seconds (demo)

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,16 @@
 let time = 0;
+let runAudio = false;
 
 setInterval(() => {
   time = time + 1;
+  if (time == 5) {
+    // chrome.action.openPopup();
+    // time = 0;
+    // runAudio = true;
+  }
   console.log(time);
 }, 1000);
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  sendResponse(time);
+  sendResponse({ time, runAudio });
+  runAudio = false;
 });
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,27 @@
+const audio = new Audio("./alarm.mp3");
+
 const timeFormatter = (timeInSeconds) => {
   const seconds = ("0" + (timeInSeconds % 60)).slice(-2);
   const minutes = Math.floor(timeInSeconds / 60);
   return `${minutes}:${seconds}`;
 };
 
-let timer = 0;
-
-setInterval(async () => {
-  timer = timer + 1;
+const setTimeDOM = async () => {
   const response = await chrome.runtime.sendMessage({ greeting: "hello" });
   console.log(response);
-  document.querySelector(".timer").innerHTML = timeFormatter(response);
+  document.querySelector(".timer").innerHTML = timeFormatter(response.time);
+  console.log(response);
+  if (response.runAudio) {
+    audio.play();
+  }
+};
+
+chrome.action.onClicked.addListener(() => {
+  console.log("clicked");
+});
+
+setInterval(async () => {
+  setTimeDOM();
 }, 1000);
+
+setTimeDOM();
